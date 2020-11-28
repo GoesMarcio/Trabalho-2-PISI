486b876bb88dcbba8988f6de4369b4980c992de4
Márcio Góes <marciopgoes@hotmail.com>
2020-11-28 13:48:23 -0300

    Interface natural

---
+++ SpaceShooter modificado/Assets/_Complete-Game/Scripts/Input/InputSource.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSource : MonoBehaviour
+{
+    public InputManager inputManager;
+    public MicrophoneInput microphone;
+
+    private void Update()
+    {
+        if (microphone.GetAveragedVolume() > 0.01)
+            inputManager.Shoot();
+
+        
+        //if (Input.GetButton("Fire2"))
+        //    inputManager.Shoot();
+        if (Input.GetKeyDown(KeyCode.R))
+            inputManager.RestartGame();
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        inputManager.MovePlayer(moveHorizontal, moveVertical);
+    }
+}
